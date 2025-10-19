e8f6c77e1d420366a543a71d0ad698f19b887f8e
Ethan Gopez <ethangopez@gmail.com>
2025-10-19 10:28:18 -0500

    Revert "updated exit user'"
    This reverts commit 39612164592ea75c7079ac7d461ef94fb3e6f3fe.

--- app/user.tsx
+++ app/user.tsx
@@ -1,10 +1,9 @@
-import { Star, X } from '@tamagui/lucide-icons';
+import { Star } from '@tamagui/lucide-icons';
 import { useState } from 'react'
 import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
 import { Button, H6, H3,H4, Paragraph, Separator, useTheme, XGroup, XStack, YStack, Avatar } from 'tamagui';
 import { useUser, CURRENT_USER_ID } from 'hooks/useApi';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useRouter } from 'expo-router';
 
 const styles = StyleSheet.create({
     imageContainer: {
@@ -73,7 +72,6 @@ function ConstellationView (){
 
 export default function UserScreen() {
     const theme = useTheme()
-    const router = useRouter()
     const points = 1000 //TODO: change later
     const imageURL = "";
     const { data } = useUser(CURRENT_USER_ID);
@@ -82,26 +80,11 @@ export default function UserScreen() {
     const addFriendClick = () => {
 
     }
-    const goToUserIndex = () => {
-        // You should use router.back() to logically "return"
-        // If you specifically want to go to /home, use router.replace('/(tabs)/home')
-        router.back(); 
-    }
     // State to track the currently selected view
     const [selectedView, setSelectedView] = useState<"constellations" | "discoveries">("constellations");
     return (
         <YStack flex={1} items="center" gap="$8" px="$10" pt="$8" bg="$background">
-            <Button
-                size="$3" // Small button size
-                chromeless // Remove background and border for a clean icon look
-                icon={<X size="$1" />} // Use the X icon
-                onPress={() => router.back()} // Use router.back() to go to the previous screen
-                position="absolute" // Position independently of the flow
-                top="$4" // Distance from the top
-                right="$4" // Distance from the right
-                zIndex={10} // Ensure it's above other elements
-                circular // Make the button circular
-            />
+
             <Avatar circular size="$10">
                 <Avatar.Image src="https://pub-495a1a79b2634ef3ae2ea1e867730068.r2.dev/33dd913e-f522-4119-8665-69113412243f" />
                 
@@ -114,6 +97,7 @@ export default function UserScreen() {
                 b="$3"
             >   
                 <H3> {name} </H3>
+                <Paragraph textAlign="center"> {id} </Paragraph>
                 <XStack
                     items="center"
                     justify="center"
